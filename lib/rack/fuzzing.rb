f7a1fbd78bb883e2b9872efa1d5a0b7cb4398604
ainame <s.namai.09@gmail.com>
2015-04-25 16:25:31 +0900

    implement Rack::Fuzzing

--- lib/rack/fuzzing.rb
+++ lib/rack/fuzzing.rb
@@ -2,12 +2,25 @@ require "rack/fuzzing/version"
 
 module Rack
   class Fuzzing
+    class << self
+      attr_accessor :irregular_responses, :condition
+
+      def confiuration(&block)
+        yield(self)
+      end
+    end
+
     def initialize(app)
       @app = app
+      yield(self.class) if block_given?
     end
 
     def call(env)
-      @app.dup.call(env)
+      if self.class.condition.respond_to?(:call) ? self.class.condition.call : self.class.condition
+        @app.dup.call(env)
+      else
+        self.class.irregular_responses.sample
+      end
     end
   end
 end
